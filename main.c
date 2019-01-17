4e49e3e8d10678c8754745a3f244400b967cd890
cfx <you@example.com>
2019-01-17 14:44:22 +0800

    结构框架

--- main.c
+++ main.c
@@ -2,8 +2,14 @@
 
 int main(){
 
-    printf("hello world!");
+    printf("欢迎进入专车管理系统\n");
 
+    printf("1--添加车辆位置\n");
+    printf("2--删除车辆位置\n");
+    printf("3--显示离我最近的车辆位置\n");
+    printf("4--返回离我最近的车辆位置\n");
+    printf("5--退出\n");
+    printf("请选择相应功能\n");
     return 0;
 
 }
